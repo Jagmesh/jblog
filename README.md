6f6a5453d6cb681facbdf09dcc35bb383e1d075b
Jagmesh <justagoodman312@gmail.com>
2024-08-10 22:45:26 +0300

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# jblog
+
+Simple Node.js colorful logger for pretty-looking console messages
+
+[![Version npm](https://img.shields.io/npm/v/jblog)](https://www.npmjs.com/package/jblog)
+
+## Installation
+
+```shell
+npm i jblog
+```
+
+## Usage
+
+```js
+import Logger from 'jblog'
+
+const logger = new Logger({
+    scopes: ['TTS', 'SERVICE']
+})
+
+const someData = 'Hello world!'
+logger.info('Well, something happened', someData)
+logger.warn('There is a chance that you haven\'t heard of the high elves')
+```
+
+![logs_screenshot.png](https://i.postimg.cc/R0jnQm3X/image-2024-08-10-22-26-30.png)
---
+++ src/index.ts
@@ -0,0 +1,72 @@
+import * as colors from 'colors'
+
+interface LogConfig {
+    /** List of scopes of logger. Will be showed in log output */
+    scopes?: string[],
+    /** UTC for time in log */
+    utc?: number
+}
+
+export default class Logger {
+    private readonly _scopeList: string | undefined;
+    private readonly _levels = {
+        INFO: 'INFO   ',
+        SUCCESS: 'SUCCESS',
+        ERROR: 'ERROR  ',
+        WARN: 'WARN   '
+    }
+    private readonly _utc: number = 0;
+
+    constructor(config?: LogConfig) {
+        if (!config) return;
+
+        this._scopeList = config.scopes
+            ? config.scopes.map((scope) => `[${scope}]`).join('')
+            : undefined
+        if (config?.utc) this._utc = config?.utc
+    }
+
+    /** Prints an INFO type log in console*/
+    public info(...text: any[]): void {
+        this._logBaseRow(colors.bgBlue(this._levels.INFO), ...text)
+    }
+
+    /** Prints a SUCCESS type log in console*/
+    public success(...text: any[]): void {
+        this._logBaseRow(colors.bgGreen(this._levels.SUCCESS), ...text)
+    }
+
+    /** Prints a WARN type log in console*/
+    public warn(...text: any[]): void {
+        this._logBaseRow(colors.bgYellow(this._levels.WARN), ...text)
+    }
+
+    /** Prints an ERROR type log in console*/
+    public error(...text: any[]): void {
+        this._logBaseRow(colors.bgRed(this._levels.ERROR), ...text)
+    }
+
+    /** Highlight a part of text*/
+    public highlight(text: string | number): string {
+        return colors.bgYellow(`${text}`)
+    }
+
+    /** Make a header from a part of text*/
+    public header(text: string | number): string {
+        return colors.cyan(`${text}`)
+    }
+
+    private _logBaseRow(level: string, ...text: any[]): void {
+        console.log(
+            colors.grey(this._getLocalDate()),
+            level + (this._scopeList ? ' ' + colors.magenta(this._scopeList) : ''),
+            ...text
+        )
+    }
+
+    private _getLocalDate(): string {
+        const date = new Date();
+        date.setHours(date.getHours() + this._utc);
+        return date.toJSON().slice(0, -5).replace(/T/, ' ');
+    }
+}
